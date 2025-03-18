d969e97a94c18be51df5863f3d5b5be73ae33952
carrinho12345 <146365108+carrinho12345@users.noreply.github.com>
2025-03-18 15:19:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function alterarFonte(idElemento, acao) {
-    let elemento = document.getElementById(idElemento);
-    let estilo = window.getComputedStyle(elemento, null).getPropertyValue('font-size');
-    let tamanhoAtual = parseFloat(estilo);
-    
-    if (acao === 'aumentar') {
-        elemento.style.fontSize = (tamanhoAtual + 2) + 'px';
-    } else if (acao === 'diminuir') {
-        elemento.style.fontSize = (tamanhoAtual - 2) + 'px';
-    }
-}
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-// Exemplo de uso:
-// <p id="texto">Este é um texto.</p>
-// <button onclick="alterarFonte('texto', 'aumentar')">Aumentar Fonte</button>
-// <button onclick="alterarFonte('texto', 'diminuir')">Diminuir Fonte</button>
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
